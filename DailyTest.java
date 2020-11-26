cf9e7b52db0336ce6499c53a4055cbb034705a95
X-Z14506 <72679349+X-Z14506@users.noreply.github.com>
2020-11-26 15:36:27 +0800

    Create DailyTest.java

---
+++ DailyTest.java
@@ -0,0 +1,39 @@
+
+
+
+
+public class IsHuiWen {
+    //先写一个方法来判断是否是回文串的方法
+    public static boolean isHuiWen(String string) {
+        int i = 0;
+        int j = string.length()-1;
+        while (i < j) {
+            if (string.charAt(i) != string.charAt(j)) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String stringA = input.nextLine();
+        String stringB = input.nextLine();
+        //记录回文串的个数
+        int count = 0;
+
+        //注意：此处的判断条件万万不可是i<stringA.length(),这样会少计算字符串最后一个字符的后面位置
+        for (int i = 0;i <= stringA.length();i++) {
+            //保证循环每次进来stringA值不变
+            StringBuilder sb = new StringBuilder(stringA);
+            //向stringA的分别不同位置插入stringB
+            sb.insert(i,stringB);
+            if (isHuiWen(stringA.toString())) {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
