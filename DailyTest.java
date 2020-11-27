8e5a1aa9e9cf67aa2c6af251dd53eca0c40037a1
X-Z14506 <72679349+X-Z14506@users.noreply.github.com>
2020-11-27 19:55:58 +0800

    Update DailyTest.java

--- DailyTest.java
+++ DailyTest.java
@@ -294,3 +294,44 @@ public class Code1  {
         }
     }
 }
+
+
+
+/*
+输入两个字符串，从第一个字符串中删除第二个字符串中的所有字符
+比如输入“They are students."
+删除之后的第一个字符串变成"The r stdnts."*/
+    
+public class Code2 {
+    //方法一：用list
+    public static void main(String[] args) {
+        List<Character> list = new ArrayList<>();
+        String str1 = "They are students";
+        String str2 = "aeiou";
+        for (int i = 0; i < str1.length();i++ ) {
+            if (!str2.contains(str1.charAt(i) +"")) {//此处要加""，因为需要的是CharSequence类型，而不是char类型
+                list.add(str1.charAt(i));
+            }
+        }
+        for (int i = 0; i < list.size();i++) {
+            System.out.print(list.get(i));
+        }
+
+    }
+
+    //方法二：用String
+    public static void main1(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String str1 = input.nextLine();
+        String str2 = input.nextLine();
+//        String str1 = "They are students";
+//        String str2 = "aeiou";
+        String ret = null;
+        for (int i = 0; i < str2.length(); i++) {
+            ret = str1.replaceAll(String.valueOf(str2.charAt(i)), "");
+            str1 = ret;
+        }
+        System.out.println(str1);
+    }
+
+
