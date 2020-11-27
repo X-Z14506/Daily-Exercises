46fbe3192177900551ebe743734ab3e588ac59e3
X-Z14506 <72679349+X-Z14506@users.noreply.github.com>
2020-11-27 19:49:24 +0800

    Update DailyTest.java

--- DailyTest.java
+++ DailyTest.java
@@ -178,3 +178,59 @@ public class Code02 {
         return k;
     }
 }
+
+
+/**
+ * 题目：读入一个字符串str，输出字符串str中的连续最长的数字串
+ * 输入描述:
+ * 个测试输入包含1个测试用例，一个字符串str，长度不超过255。
+ *
+ * 输出描述:
+ * 在一行内输出str中里连续最长的数字串。
+ * 示例1
+ * 输入
+ *
+ * abcd12345ed125ss123456789
+ * 输出
+ *
+ * 123456789
+ *
+ * 思路：用两个循环判断，一个判断是否为数字开始，第二个计算长度，最后将最长的从原字符串中截取出来
+ */
+public class Code1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String str = input.nextLine();
+        String ret = "";
+        int count = 0;
+        int index = 0;
+        for (int i = 0; i < str.length();i++) {
+            //出现第一个数字
+            if (str.charAt(i) >= '0'&& str.charAt(i) <= '9') {
+                count = 1;//先给计数器加1
+                index = i;//定位第一个数字出现的下标
+                for (int j = i ; j < str.length();j++) {
+                    if (str.charAt(j) >= '0' && str.charAt(j) <= '9') {
+                        count++;
+                        index = j;
+                    }else {
+                        break;
+                    }
+                }
+                if (count > ret.length()) {
+                    ret = str.substring(i,index+1);//substring(0,5)左闭右开，表示包含0号下标，不包含5号下标
+                }
+            }else {
+                continue;
+            }
+        }
+        System.out.println(ret);
+    }
+}
+
+
+
+
+
+
+
