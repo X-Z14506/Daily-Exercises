31e3ef78eecf5b08e015f1ee78ed8d68b37da65f
X-Z14506 <72679349+X-Z14506@users.noreply.github.com>
2020-12-03 11:13:34 +0800

    Update DailyTest.java

--- DailyTest.java
+++ DailyTest.java
@@ -590,3 +590,25 @@ public class Main {
         System.out.println(new String(data));
     }
 }
+
+    
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    
+//输入一个正整数n，求n！末尾有多少个0？比如：n=10;n!=32628800,输出为2
+public class code {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        Long n = input.nextLong();
+        Long ret = 1l;
+        for (int i = 1; i<=n;i++) {
+             ret *= i;
+        }
+//        System.out.println(ret);
+        int count = 0;
+        while (ret%10 == 0){
+            ret /= 10;
+            count++;
+        }
+        System.out.println(count);
+    }
+}
