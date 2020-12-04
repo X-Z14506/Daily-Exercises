7c268a25f989fed1ead6effb41b818fa368f5e40
X-Z14506 <72679349+X-Z14506@users.noreply.github.com>
2020-12-04 09:51:21 +0800

    Update DailyTest.java

--- DailyTest.java
+++ DailyTest.java
@@ -784,3 +784,34 @@ public class Code1 {
 }
 
     ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    
+    
+/*
+给定一个数组A[0,1,...,n-1],请构建一个数组B[0,1,...,n-1],其中B中的元素B[i]=A[0]*A[1]*...*A[i-1]*A[i+1]*...*A[n-1]。不能使用除法
+ */
+public class Code2 {
+    public static int[] multiply(int[] A) {
+        int[] b = new int[A.length];
+        for (int i = 0;i < A.length;i++) {
+            int sum = 1;
+            for (int j = 0 ;j < A.length;j++) {
+                if (j == i) {
+                    continue;
+                }else {
+                    sum*=A[j];
+                }
+            }
+            System.out.println(sum);
+            b[i]=sum;
+        }
+        return b;
+    }
+
+    public static void main(String[] args) {
+        int[] a = {1,2,3,4,5};
+        int[] ret = multiply(a);
+        System.out.println(Arrays.toString(ret));
+    }
+}
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
