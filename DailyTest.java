ee55ce0c9740da2bb17f8641a19a3a141f0784e4
X-Z14506 <72679349+X-Z14506@users.noreply.github.com>
2020-12-06 10:20:54 +0800

    Update DailyTest.java

--- DailyTest.java
+++ DailyTest.java
@@ -991,3 +991,29 @@ public class Main {
 
 
 }
+    
+    
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    
+    /*
+     交换数组0号下标和1号下标的值
+ */
+public class Code1 {
+    public static void main(String[] args) {
+        int[] a = {1,2};
+        System.out.println(Arrays.toString(a));
+        int[] b = exchangeAB(a);
+        System.out.println(Arrays.toString(b));
+    }
+    /*
+    交换前 [1, 2]
+    交换后 [2, 1]
+    */
+    public static int[] exchangeAB(int[] AB) {
+        int temp = AB[0];
+        AB[0] = AB[1];
+        AB[1] = temp;
+        return AB;
+    }
+}
+
