1cc9b4921325829c1fa65986baf30c20c705c3de
X-Z14506 <72679349+X-Z14506@users.noreply.github.com>
2021-01-23 17:42:40 +0800

    Update DailyTest.java

--- DailyTest.java
+++ DailyTest.java
@@ -1,6 +1,49 @@
 
 
 
+
+
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+/*
+反转链表
+ */
+public class Main16 {
+    public ListNode16 ReverseList(ListNode16 head) {
+        ListNode16 cur = head;
+        ListNode16 prev = null;
+        ListNode16 newHead = null;
+        while (cur != null) {
+            //每次循环刚进来需要开辟一个节点保存cur.next，
+            // 因为要让cur指向cur前一个节点，不保存cur.next就会找不到cur.next
+            ListNode16 curNext = cur.next;
+            if (cur.next == null) {
+                newHead = cur;
+            }
+            cur.next = prev;
+            prev = cur;
+            cur = curNext;
+        }
+        return newHead;
+    }
+}
+class ListNode16 {
+    int val;
+    ListNode16 next = null;
+
+    ListNode16(int val) {
+        this.val = val;
+    }
+
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
 /*
 题目描述：
 给定一个double类型的浮点数base和int类型的整数exponent。求base的exponent次方。
