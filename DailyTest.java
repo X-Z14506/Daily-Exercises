a76ddcc58ddcf0cef9792deea919b4f3740fafe5
X-Z14506 <72679349+X-Z14506@users.noreply.github.com>
2021-01-29 18:13:58 +0800

    Update DailyTest.java

--- DailyTest.java
+++ DailyTest.java
@@ -3,6 +3,190 @@
 
 
 
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+/*
+序列中删除指定数字（所有）
+
+输入：6
+     1 2 3 4 5 9
+     4
+
+输出：1 2 3 5 9
+
+输入：10
+     1 1 2 3 4 4 5 4 9 4
+     4
+输出：1 1 2 3 5 9
+ */
+
+public class Main28 {
+    //遍历数组，不需要删除的直接输出，需要删除的元素，continue，跳过输出语句
+    public static void main2(String[] args) throws IOException{
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(bf.readLine());
+
+        int[] ns = new int[n];
+        String[] str = bf.readLine().split(" ");
+        for(int i = 0; i < n; i++){
+            ns[i] = Integer.parseInt(str[i]);
+        }
+
+        int s = Integer.parseInt(bf.readLine());//要删除的整数
+        for(int i = 0; i < ns.length; i++){
+            if(ns[i] == s){
+                continue;
+                //当条件 ns[i]==s 的时候执行 continue 语句，
+                //continue 语句会终止本次循环，循环体中 continue 之后的语句将不再执行，
+                //接着进行下次循环，所以输出结果中没有 s。
+                //也就相当于删除指定数字
+            }
+            System.out.print(ns[i] + " ");
+        }
+
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(reader.readLine());
+        String[] strings = reader.readLine().split(" ");
+        int m = Integer.parseInt(reader.readLine());
+        List<Integer> list = new ArrayList<>();
+        int j = 0;
+        for (int i = 0;i < strings.length;i++) {
+            int k = Integer.parseInt(strings[i]);
+            //如果k不为要删除的数，放入ints数组
+            if ( k != m) {
+                list.add(k);
+            }
+        }
+        for (int i : list) {
+            System.out.print(i+" ");
+        }
+    }
+}
+
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+/*
+输入：5
+     1 6 9 22 30
+     8
+
+输出：1 6 8 9 22 30
+
+ */
+public class Main27 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String s = reader.readLine();
+        int n = Integer.parseInt(s);
+        String[] strings = reader.readLine().split(" ");
+        int[] ints = new int[n+1];
+        for (int i = 0;i < strings.length;i++) {
+            ints[i] = Integer.parseInt(strings[i]);
+        }
+        int m = Integer.parseInt(reader.readLine());
+        ints[n] = m;
+        Arrays.sort(ints);
+        for (int i = 0;i < ints.length;i++) {
+            System.out.print(ints[i]+" ");
+        }
+    }
+}
+
+
+
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/*
+
+题目描述
+输入一个整数序列，判断是否是有序序列，有序，指序列中的整数从小到大排序或者从大到小排序。
+输入描述:
+第一行输入一个整数N(3≤N≤50)。
+第二行输入N个整数，用空格分隔N个整数。
+输出描述:
+输出为一行，如果序列有序输出sorted，否则输出unsorted。
+
+
+输入：5
+     1 6 9 22 30
+输出：sorted
+
+输入：5
+     3 4 7 2 10
+输出：unsorted
+
+ */
+
+    //注：升序降序都是有序输出，两种情况都要考虑，不能只考虑一种
+public class Main26 {
+        public static void main1(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            while (sc.hasNext()) {
+                int n = sc.nextInt();
+                int pre = sc.nextInt();
+                int flag = 0;
+                boolean judge = true;
+                for (int i = 1; i < n; i++) {
+                    int cur = sc.nextInt();
+                    //判断是否全部是升序的
+                    if (cur - pre >= 0 && flag>=0){
+                        flag = 1;
+                     //判断是否全部是降序
+                    }else if(cur - pre <= 0 && flag<=0){
+                        flag = -1;
+                    //不是全部升或者降，将判断标志改为false
+                    }else{
+                        judge = false;
+                    }
+                    pre = cur;
+                }
+                if (judge){
+                    System.out.println("sorted");
+                }else {
+                    System.out.println("unsorted");
+                }
+            }
+        }
+    //如果一个数比两边的数都大，或者比两边的数都小，则不是有序的，利用数学单调性的思想
+    public static void main3(String[] args) throws IOException{
+        // TODO Auto-generated method stub
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        int n=Integer.parseInt(br.readLine());
+        String[] str=br.readLine().split(" ");
+        int []a = new int[n];
+        for(int i = 0; i < str.length; i++) {
+            a[i] = Integer.parseInt(str[i]);
+        }
+        int flag = 0;
+        for(int i=1;i<str.length-1;i++) {
+            if((a[i]>a[i-1]&&a[i]>a[i+1])||(a[i]<a[i-1]&&a[i]<a[i+1])) {
+                flag = 1;
+            }
+        }
+        if(flag == 1) {
+            System.out.println("unsorted");
+        }
+        else {
+            System.out.println("sorted");
+        }
+    }
+
+}
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
 public class Main25 {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
