37dd6a315dc212ef4a1b3085cedd2626edb20d39
X-Z14506 <72679349+X-Z14506@users.noreply.github.com>
2021-01-29 18:14:37 +0800

    Update DailyTest.java

--- DailyTest.java
+++ DailyTest.java
@@ -3,6 +3,49 @@
 
 
 
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+/*
+序列中整数去重
+保证按照输入的顺序输出
+输入：5
+     10 12 93 12 75
+
+输出：10 12 93 75
+
+ */
+
+public class Main29 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(reader.readLine());
+        String[] strings = reader.readLine().split(" ");
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0;i < strings.length;i++) {
+            if (!list.contains(Integer.parseInt(strings[i]))) {
+                list.add(Integer.parseInt(strings[i]));
+            }
+        }
+        for (int i : list) {
+            System.out.print(i+" ");
+        }
+
+       /* //不能用HashSet 或者 TreeSet，因为要按照输入的顺序输出
+        Set<Integer> set = new HashSet<>();
+        for (int i=0 ;i < strings.length;i++) {
+            set.add(Integer.parseInt(strings[i]));
+        }
+        System.out.println(set);*/
+    }
+}
+
+
+
+
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
