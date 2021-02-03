8f0a2cb96db31fa008d0dd73c11b768020aaff2d
X-Z14506 <72679349+X-Z14506@users.noreply.github.com>
2021-02-03 15:09:34 +0800

    Update DailyTest.java

--- DailyTest.java
+++ DailyTest.java
@@ -1,6 +1,198 @@
 
 
 
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+/*
+有这样一道智力题：“某商店规定：三个空汽水瓶可以换一瓶汽水。小张手上有十个空汽水瓶
+ 她最多可以换多少瓶汽水喝？”答案是5瓶，方法如下：先用9个空瓶子换3瓶汽水，喝掉3瓶满的，
+ 喝完以后4个空瓶子，用3个再换一瓶，喝掉这瓶满的，这时候剩2个空瓶子。然后你让老板先借给你一瓶汽水
+ 喝掉这瓶满的，喝完以后用3个空瓶子换一瓶满的还给老板。如果小张手上有n个空汽水瓶，最多可以换多少瓶汽水喝？
+
+输入描述:
+输入文件最多包含10组测试数据，每个数据占一行，仅包含一个正整数n（1<=n<=100），
+ 表示小张手上的空汽水瓶数。n=0表示输入结束，你的程序不应当处理这一行。
+
+输出描述:
+对于每组测试数据，输出一行，表示最多可以喝的汽水瓶数。如果一瓶也喝不到，输出0。
+
+输入例子:
+
+3
+10
+81
+0
+输出例子:
+1
+5
+40
+*/
+
+public class Code02 {
+    public static void main(String[] args) {
+        Scanner input =  new Scanner(System.in);
+        while (input.hasNext()) {
+            int num = input.nextInt();
+            if (num!=0) {
+                int ret = maxDrinks(num);
+                System.out.println(ret);
+            }
+        }
+    }
+
+    private static int maxDrinks(int num) {
+        //先将手上的空瓶子三个一组分好，
+        int k = 0;
+        while (num >= 3) {
+            //1.空瓶子为三的倍数时，k为可以换回饮料的个数
+            if (num % 3 == 0) {
+                k = k + num/3;
+                num = num/3;
+            }
+            //2.空瓶子不为三的个数时，num为空瓶子数
+            if (num % 3 != 0) {
+                k= k + num/3;
+                int i = num%3;
+                num = num/3 + i;
+            }
+        }
+        //3.空瓶子数为2时，还可以换一瓶饮料，所以k+1；
+        if (num == 2) {
+            k = k+1;
+        }
+        return k;
+    }
+}
+
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+/*有一组数，对于其中任意两个数组，若前面一个大于后面一个数字，
+ 则这两个数字组成一个逆序对，请设计一个高效的算法，计算给定数组中的逆序对个数。*/
+
+public class Code01 {
+    public static void main(String[] args) {
+        int[] A = {1,2,3,4,5,6,7,0};
+        int n = 8;
+        System.out.println(count(A, n));//7
+    }
+    public static int count(int[] A, int n) {
+        int count = 0;
+        for (int i = 0; i < n;i++) {
+            for (int j = i+1;j < n;j++) {
+                if (A[i] > A[j]) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
+
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] a = {1,3,5,2,2};
+        System.out.println(findKth(a, 5, 3));//2
+    }
+    /*有一个整数数组，请你根据快速排序的思路，找出数组中第K大的数。
+
+    给定一个整数数组a,同时给定它的大小n和要找的K(K在1到n之间)，请返回第K大的数，保证答案存在。
+    测试样例：[1,3,5,2,2],5,3
+        返回：2*/
+    public static int findKth(int[] a, int n, int K) {
+        return find(0,n-1,a,K);
+    }
+    public static int find(int left,int right, int[] arr,int k) {
+        int ret = partation(left,right,arr);
+        if (ret+1>k) {
+            return find(left,ret-1,arr,k);
+        }else if (ret+1<k) {
+            return find(ret+1,right,arr,k);
+        }else {
+            return arr[ret];
+        }
+    }
+    public static int partation(int left,int right,int[] arr) {
+        int temp = arr[left];
+        while (left < right) {
+            //设定arr[left]为基准，将小于基准的放在基准右边,大于基准的放在左边
+            while (left < right && arr[right] <= temp) {
+                right--;
+            }
+            if (left>=right) {
+                break;
+            }else {
+                arr[left] = arr[right];//将大于基准的值放在基准左边
+            }
+            while (left < right && arr[left] >= temp) {
+                left++;
+            }
+            if (left >= right) {
+                break;
+            }else {
+                arr[right] = arr[left];//将小于基准的值放在基准右边
+            }
+        }
+        //将基准最后放再最后一个空位置，这个空位置的下标即为基准的下标
+        arr[left] = temp;
+        return left;
+    }
+}
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+public class IsHuiWen {
+    //先写一个方法来判断是否是回文串的方法
+    public static boolean isHuiWen(String string) {
+        int i = 0;
+        int j = string.length()-1;
+        while (i < j) {
+            if (string.charAt(i) != string.charAt(j)) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String stringA = input.nextLine();
+        String stringB = input.nextLine();
+        //记录回文串的个数
+        int count = 0;
+
+        //注意：此处的判断条件万万不可是i<stringA.length(),这样会少计算字符串最后一个字符的后面位置
+        for (int i = 0;i <= stringA.length();i++) {
+            //保证循环每次进来stringA值不变
+            StringBuilder sb = new StringBuilder(stringA);
+            //向stringA的分别不同位置插入stringB
+            sb.insert(i,stringB);
+            if (isHuiWen(stringA.toString())) {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 
 /*
             题目描述：
