9255d9fa85b052731453ae78786ef29f6017e25b
X-Z14506 <72679349+X-Z14506@users.noreply.github.com>
2021-03-21 17:11:50 +0800

    剑指offer

---
+++ offer/变态跳台阶.java
@@ -0,0 +1,18 @@
+/*
+变态跳台阶
+一只青蛙一次可以跳上1级台阶，也可以跳上2级……它也可以跳上n级。求该青蛙跳上一个n级的台阶总共有多少种跳法。
+ */
+
+//每个台阶都有跳与不跳两种情况（除了最后一个台阶），最后一个台阶必须跳。所以共用2^(n-1)中情况
+public class 变态跳台阶 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            int n = sc.nextInt();
+            System.out.println(JumpFloorII(n));
+        }
+    }
+    public static int JumpFloorII(int target) {
+        return (int)Math.pow(2,target-1);
+    }
+}
